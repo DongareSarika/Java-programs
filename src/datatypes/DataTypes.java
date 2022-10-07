a3312918d0563c9b023388ca2f2aabb94a1aab19
Lenovo <Lenovo@LAPTOP-D18QERK9>
2022-10-08 01:27:55 +0530

    Program for data types

---
+++ src/datatypes/DataTypes.java
@@ -0,0 +1,24 @@
+package datatypes;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		byte b= (byte)300; // cascading
+		System.out.println("b=" + b);
+		long l= 456789l;
+		System.out.println("l=" + l);
+		float f= 234.5f;
+		System.out.println("f=" + f);
+		
+		
+		char ch= 'A';
+		int ValCh= (int)'A';
+		System.out.println("Ascci value of A=" + ValCh);
+		
+	}
+	
+
+}
+//byte,int, short, long
+// float, double
+//boolean
+//char
